1857a2150e99b4c528b3a830a73f420d3d848e5c
sarthakg043 <sarthakg4953@gmail.com>
2023-02-14 23:16:51 +0530

    added music

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+// Howler Js starts here
+var bgmusic = new Howl({
+  src: ["music.mp3"],
+  // The above audio is taken from https://www.youtube.com/watch?v=kBsUwIfL8kU
+  preload: true,
+  onload: function(){
+      console.log("Music has loaded");
+  },
+  onloaderror: function(){
+      console.log("Music can't be loaded");
+  },
+  onplay: function(){
+      console.log("Music has started");
+  },
+  onplayerror: function(){
+      console.log("Music can't be played");
+  },
+  autoplay: true,
+  volume: 1,
+  loop: true,
+  onend: function(){
+      console.log("Music ended");
+  }	
+});
+
+
 window.onload = function() {
 
+  bgmusic.play();
     function getR() {
       var W = window.innerWidth;
       var H = window.innerHeight;
